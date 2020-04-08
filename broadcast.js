5ad7fb5cbdec6fa0dbe5e7c9f952f31d7a717e17
marchuanv <marchuanv@gmail.com>
2020-04-08 15:17:50 +0200

    refactor

--- broadcast.js
+++ broadcast.js
@@ -1,10 +1,14 @@
 const messagebus = require("messagebus");
+const utils = require("utils");
+
 messagebus.start( {port: process.env.PORT || 3000});
 const subscription = messagebus.subscribe( { messageId: "broadcast", urlPath: "/broadcast", contentType: "application/json" });
 subscription.onreceive = async (broadcastInfo) => {
+    utils.log("Broadcast","received broadcast information");
     const broadSub = messagebus.subscribe( { messageId: broadcastInfo.channel, urlPath: `/${broadcastInfo.channel}`, contentType: broadcastInfo.contentType });
     broadSub.onreceive = () => {};
     for (const address of broadcastInfo.remoteAddresses){
+        utils.log("Broadcast",`broadcasting data for ${broadcastInfo.channel} to ${address.host}:${address.port}`);
         await messagebus.publish({ messageId: broadcastInfo.channel, host: address.host, port: address.port, data: broadcastInfo.data });
     };
 };
