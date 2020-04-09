60b795ea2377aa848f31235872dd35d792f5fb2f
marchuanv <marchuanv@gmail.com>
2020-04-09 12:22:47 +0200

    refactor

--- broadcast.js
+++ broadcast.js
@@ -2,13 +2,14 @@ const messagebus = require("messagebus");
 const utils = require("utils");
 
 messagebus.start( {port: process.env.PORT || 3000});
-const subscription = messagebus.subscribe( { messageId: "broadcast", urlPath: "/broadcast", contentType: "application/json" });
-subscription.onreceive = async (broadcastInfo) => {
-    utils.log("Broadcast","received broadcast information");
-    const broadSub = messagebus.subscribe( { messageId: broadcastInfo.channel, urlPath: `/${broadcastInfo.channel}`, contentType: broadcastInfo.contentType });
+const subscription = messagebus.subscribe( { path: "/broadcast", contentType: "application/json" });
+subscription.onreceive = async (broadcastMessage) => {
+    utils.log("Broadcast","------------------------------------< BROADCAST RECEIVED START >----------------------------------------\r\n");
+    const broadSub = messagebus.subscribe( { path: broadcastMessage.path, contentType: broadcastMessage.contentType });
     broadSub.onreceive = () => {};
-    for (const address of broadcastInfo.remoteAddresses){
-        utils.log("Broadcast",`broadcasting data for ${broadcastInfo.channel} to ${address.host}:${address.port}`);
-        await messagebus.publish({ messageId: broadcastInfo.channel, host: address.host, port: address.port, data: broadcastInfo.data });
+    for (const subscriber of broadcastMessage.subscribers){
+        utils.log("Broadcast",`broadcasting message to ${subscriber.host}:${subscriber.port}${broadcastMessage.path}`);
+        await messagebus.publish({ path: broadcastMessage.path, host: subscriber.host, port: subscriber.port, data: broadcastMessage.data });
     };
+    utils.log("Broadcast","--------------------------------------------------------------------------------------------------------\r\n");
 };
