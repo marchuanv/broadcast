163a54ea1bdd1e5ddd8d8b63b94371163423751c
marchuanv <marchuanv@gmail.com>
2020-04-16 14:53:09 +0200

    ready for hosting

--- broadcast.js
+++ broadcast.js
@@ -1,6 +1,10 @@
 const messagebus = require("messagebus");
 const utils = require("utils");
-messagebus.subscribe( { host: "localhost", port: 3000, path: "/broadcast", contentType: "application/json" }).callback = async({ username, passphrase, path, contentType, content, subscribers }) => {
+const host = process.env.HOST || "localhost";
+const port = process.env.PORT || 3000;
+const path = "/broadcast";
+const contentType = "application/json";
+messagebus.subscribe( { host, port, path, contentType }).callback = async({ username, passphrase, path, contentType, content, subscribers }) => {
     utils.log("Broadcast","------------------------------------< BROADCAST RECEIVED START >----------------------------------------\r\n");
     for (const subscriber of subscribers){
         utils.log("Broadcast",`broadcasting message to ${subscriber.host}:${subscriber.port}${path}`);
