24bb72bf0fcb8dcc72f6b51d78339067e0eee64e
marchuanv <marchuanv@gmail.com>
2020-04-16 10:09:38 +0200

    refactor

--- broadcast.js
+++ broadcast.js
@@ -1,14 +1,13 @@
 const messagebus = require("messagebus");
 const utils = require("utils");
-
-const subscription = messagebus.subscribe( { path: "/broadcast", contentType: "application/json" });
-subscription.onreceive = async (broadcastMessage) => {
+messagebus.subscribe( { host: "localhost", port: 3000, path: "/broadcast", contentType: "application/json" }).callback = async({ path, contentType, data, subscribers }) => {
     utils.log("Broadcast","------------------------------------< BROADCAST RECEIVED START >----------------------------------------\r\n");
-    const broadSub = messagebus.subscribe( { path: broadcastMessage.path, contentType: broadcastMessage.contentType });
-    broadSub.onreceive = () => { utils.log("Broadcast",`server will not handle messages for ${broadSub.path}, refer to the component handle these messages`);};
-    for (const subscriber of broadcastMessage.subscribers){
-        utils.log("Broadcast",`broadcasting message to ${subscriber.host}:${subscriber.port}${broadcastMessage.path}`);
-        await messagebus.publish({ path: broadcastMessage.path, host: subscriber.host, port: subscriber.port, data: broadcastMessage.data });
+    messagebus.subscribe( { path, contentType }).callback = () => {
+        utils.log("Broadcast",`server will not handle messages for ${broadSub.path}, refer to the messagebus that handles these messages`);
+    };
+    for (const subscriber of subscribers){
+        utils.log("Broadcast",`broadcasting message to ${subscriber.host}:${subscriber.port}${path}`);
+        await messagebus.publish({ path, host: subscriber.host, port: subscriber.port, data });
     };
     utils.log("Broadcast","--------------------------------------------------------------------------------------------------------\r\n");
 };
