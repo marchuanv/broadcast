e72b6f3e2819fcc51c1946f8374845cafe3f72f0
marchuanv <marchuanv@gmail.com>
2020-04-16 12:14:14 +0200

    broadcast working underlying components still have bugs

--- broadcast.js
+++ broadcast.js
@@ -1,13 +1,10 @@
 const messagebus = require("messagebus");
 const utils = require("utils");
-messagebus.subscribe( { host: "localhost", port: 3000, path: "/broadcast", contentType: "application/json" }).callback = async({ path, contentType, data, subscribers }) => {
+messagebus.subscribe( { host: "localhost", port: 3000, path: "/broadcast", contentType: "application/json" }).callback = async({ username, passphrase, path, contentType, content, subscribers }) => {
     utils.log("Broadcast","------------------------------------< BROADCAST RECEIVED START >----------------------------------------\r\n");
-    messagebus.subscribe( { path, contentType }).callback = () => {
-        utils.log("Broadcast",`server will not handle messages for ${broadSub.path}, refer to the messagebus that handles these messages`);
-    };
     for (const subscriber of subscribers){
         utils.log("Broadcast",`broadcasting message to ${subscriber.host}:${subscriber.port}${path}`);
-        await messagebus.publish({ path, host: subscriber.host, port: subscriber.port, data });
+        await messagebus.publish({ host: subscriber.host, port: subscriber.port, path, username, passphrase, contentType, content });
     };
-    utils.log("Broadcast","--------------------------------------------------------------------------------------------------------\r\n");
+    utils.log("Broadcast","------------------------------------< BROADCAST RECEIVED END >----------------------------------------\r\n");
 };
