dbe82f594a079ae5f76219e0b5bdcb1122208bcb
marchuanv <marchuanv@gmail.com>
2020-04-29 17:25:09 +0200

    refactor

--- broadcast.js
+++ broadcast.js
@@ -10,7 +10,6 @@ const passphrase = process.env.PASSPHRASE || "secure1";
 let services = [];
 
 messagebus.subscribe( { host, port, path: broadcastPath, contentType }).callback = async({ path, contentType, content }) => {
-    utils.log("Broadcast","------------------------------------< BROADCAST START >----------------------------------------\r\n");
     for (const service of services.filter(s => s.path === path)){
         const url = `${service.host}:${service.port}${service.path}`;
         utils.log("Broadcast",`publishing message to ${url}`);
@@ -24,21 +23,16 @@ messagebus.subscribe( { host, port, path: broadcastPath, contentType }).callback
             content
         });
     };
-    utils.log("Broadcast","------------------------------------< BROADCAST END >----------------------------------------\r\n");
-    return `all remote services that subscribe to ${path} have been notified.`;
+    return;
 };
 
 messagebus.subscribe( { host, port, path: registerPath, contentType }).callback = async({ host, port, path }) => {
-    utils.log("Broadcast Register","------------------------------------< BROADCAST REGISTER START >----------------------------------------\r\n");
     if (!host || !port || !path){
-        const message = "failed to register request, host, port and path is not in the message.";
-        utils.log("Broadcast", message);
-        throw new Error(message);
+        utils.error("Broadcast Register", "failed to register request, host, port and path is not in the message.");
     }
     services =  services.filter(s => s.host !== host && s.port !== port && s.path !== path);
     const url = `${host}:${port}${path}`;
     utils.log("Broadcast Register",`adding ${url} to the list of known services`);
     services.push({ host, port, path });
-    utils.log("Broadcast Register","------------------------------------< BROADCAST REGISTER END >----------------------------------------\r\n");
     return `${host}:${port}${path} registered`;
 };
